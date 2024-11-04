74d2ac07d3c6bd9572774b7d8e69a218a88d89a0
Michael Primeaux <mprimeaux@users.noreply.github.com>
2024-11-03 21:41:23 -0600

    Update nanoid.go

--- nanoid.go
+++ nanoid.go
@@ -247,7 +247,7 @@ func NewGenerator(options ...Option) (Generator, error) {
 	// Initialize buffer pools based on Rune handling
 	pool := &sync.Pool{
 		New: func() interface{} {
-			buf := make([]byte, 0, runtimeConfig.bufferSize)
+			buf := make([]byte, runtimeConfig.bufferSize)
 			return &buf
 		},
 	}
